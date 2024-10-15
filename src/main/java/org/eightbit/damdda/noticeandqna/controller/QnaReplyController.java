292927a4128c4e8c645bb062ea7a19bb36e6fece
seyeonPark3377 <dydtpsp0824@naver.com>
2024-10-15 21:33:38 +0900

    feat: 큐앤에이 답글 컨트롤러 구현

--- src/main/java/org/eightbit/damdda/noticeandqna/controller/QnaReplyController.java
+++ src/main/java/org/eightbit/damdda/noticeandqna/controller/QnaReplyController.java
@@ -1,31 +1,44 @@
 package org.eightbit.damdda.noticeandqna.controller;
 
-import org.eightbit.damdda.noticeandqna.domain.QnaQuestion;
+import io.swagger.v3.oas.annotations.Operation;
 import org.eightbit.damdda.noticeandqna.dto.QnaQuestionDTO;
+import org.eightbit.damdda.noticeandqna.dto.QnaReplyDTO;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/damdda/qna/question")
 public class QnaReplyController {
 
     @PostMapping
-    public ResponseEntity<String> registerQna(){
+    @Operation(summary = "Q&A 답글 생성", description = "Q&A 질문에 대한 답글을 생성합니다.")
+    public ResponseEntity<QnaReplyDTO> registerQna(@RequestBody QnaReplyDTO qnaReplyDTO) {
+        System.out.println(qnaReplyDTO);
         return null;
     }
 
     @GetMapping
-    public ResponseEntity<String> getQna(){
+    @Operation(summary = "Q&A 답글 조회", description = "Q&A 질문에 대한 답글을 조회합니다.")
+    public ResponseEntity<Page<QnaReplyDTO>> getQna(@RequestParam String parentReplyId) {
+        System.out.println(parentReplyId);
         return null;
     }
 
     @DeleteMapping("/{qnaQuestionId}")
-    public ResponseEntity<String> deleteQna(@PathVariable("qnaQuestionId") String qnaQuestionId){
+    @Operation(summary = "Q&A 답글 삭제", description = "Q&A 질문에 대한 답글을 삭제합니다.")
+    public ResponseEntity<Map<String, Boolean>> deleteQna(@PathVariable("qnaQuestionId") String parentReplyId){
+        System.out.println(parentReplyId);
         return null;
     }
 
     @PutMapping("/{qnaQuestionId}")
-    public ResponseEntity<String> modifyQna(@RequestBody QnaQuestionDTO qnaQuestionDTO, @PathVariable("qnaQuestionId") String qnaQuestionId){
+    @Operation(summary = "Q&A 답글 수정", description = "Q&A 질문에 대한 답글을 수정합니다.")
+    public ResponseEntity<QnaReplyDTO> modifyQna(@RequestBody QnaReplyDTO qnaReplyDTO, @PathVariable("qnaQuestionId") String parentReplyId){
+        System.out.println(parentReplyId);
+        System.out.println(qnaReplyDTO);
         return null;
     }
 }
